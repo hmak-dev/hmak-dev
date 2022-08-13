4a04576c1012e7109e40c8dec91353f1d5c9d04d
Hossein Maktoobian <8bitsight@gmail.com>
2022-08-13 14:55:56 +0430

    improve build script

--- scripts/build.js
+++ scripts/build.js
@@ -1,22 +1,43 @@
 const fs = require('fs');
 const esbuild = require('esbuild');
-const { sassPlugin } = require('esbuild-sass-plugin');
+const {sassPlugin} = require('esbuild-sass-plugin');
+const minimist = require('minimist');
 
-fs.mkdirSync('build');
+const args = minimist(process.argv);
 
-console.log('[1] Copying HTML File...');
+if (!fs.existsSync('build')) {
+	fs.mkdirSync('build');
+}
+
+console.log('- Copying HTML File...');
 fs.copyFileSync('src/index.html', 'build/index.html');
 
-console.log('[2] Bundling Scripts...');
-esbuild.build({
+const config = {
 	entryPoints: ['src/main.ts', 'src/main.sass'],
 	outdir: 'build',
-	watch: true,
 	bundle: true,
 	minify: true,
 	keepNames: false,
 	legalComments: 'none',
 	plugins: [sassPlugin()],
-}).then(() => {
-	console.log('[X] Build Finished Successfully');
-})
+};
+
+if (args.serve) {
+	console.log('- Starting Server...');
+
+	esbuild.serve({
+		servedir: 'build',
+		host: args.host || '0.0.0.0',
+		port: args.port || 8000,
+	}, config).then(() => {
+		console.log('+ Server Started at http://0.0.0.0:8000');
+	})
+} else {
+	config.watch = args.watch;
+
+	console.log('- Bundling...');
+
+	esbuild.build(config).then(() => {
+		console.log('+ Build Finished Successfully');
+	})
+}
